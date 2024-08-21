5a7b7f3dca4f590fa42f5d750d2f0e6d9eab2785
Kate Semushkina <ssemushkinaes@mail.ru>
2024-08-21 21:58:53 +0300

    Место js.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function createCard(cardData, deleteCallBack){
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => {
+    deleteCallBack(cardElement)
+    });
+    
+    cardList.append(cardElement);
+}
+
 // @todo: Функция удаления карточки
+function deleteCard(cardElement){
+   cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+for (let i = 0; i < initialCards.length; i++){
+    createCard(initialCards[i],deleteCard);
+}
+
